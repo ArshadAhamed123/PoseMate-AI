93bc629a3a1001a5e193dcce781150a2a90a753c
Arshad Ahamed N <ahamedasquares@gmail.com>
2026-08-07 20:27:18 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+THRESHOLD_ANGLE = 15
+THRESHOLD_ALIGNMENT = 0.05  # for x-axis alignment
+
+JOINTS = {
+    "left_elbow": (11, 13, 15),
+    "right_elbow": (12, 14, 16),
+    "left_knee": (23, 25, 27),
+    "right_knee": (24, 26, 28)
+}
